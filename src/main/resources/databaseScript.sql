15b795dd958b4c8ecef64583e8b5a9a78fbbe4d5
albo0003 <albo0003@stud.kea.dk>
2025-03-31 12:46:16 +0200

    Update databaseScript.sql

--- src/main/resources/databaseScript.sql
+++ src/main/resources/databaseScript.sql
@@ -18,7 +18,7 @@ CREATE TABLE cars (
 
 -- Dummy data, Det er de samme objekter som bruges i InitData.
 INSERT INTO cars (brand, modelyear, type, colour, licenseplate, img) VALUES
-                    ('Volvo', 2012, 'Sedan', 'Red', 'ABC123', 'blåvolvostationcar.jpg'),
+                    ('Volvo', 2012, 'Sedan', 'brown', 'ABC123', 'blåvolvostationcar.jpg'),
                     ('Ford', 1938, 'SUV', 'Blue', 'DEF456', 'generic.jpg'),
                     ('VW', 1986, 'Coupe', 'Black', 'GHI789', 'GulVWBobbel.jpg'),
                     ('Western Star', 1998, 'Hatchback', 'White', 'JKL012', 'WesternstarRødTruck.jpg'),
